fe0e89445a561c50826a8a24b9011fd947667c43
Valeriy <valerkinch@gmail.com>
2014-04-21 20:04:12 +0600

    Update jquery.datetimepicker.js

--- jquery.datetimepicker.js
+++ jquery.datetimepicker.js
@@ -135,7 +135,7 @@
 					"日", "月", "火", "水", "木", "金", "土"
 				]
 			},
-			en:{ // Vietnamese
+			vi:{ // Vietnamese
 				months: [
 					"Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"
 				],
